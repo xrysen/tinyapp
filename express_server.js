ca0c4d5d342aa7489c2b1dd55f0e59b884ac0b05
Sean Oyler <xrysen@gmail.com>
2020-11-10 23:33:17 +0000

    Created basic layout for registration page

--- express_server.js
+++ express_server.js
@@ -93,6 +93,14 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
+// Registration Page
+app.get("/register", (req, res) => {
+  const templateVars = {
+    username: req.cookies['username']
+  }
+  res.render("register", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
