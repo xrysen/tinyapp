25043999098db27f033ae8c36793f2fb4f59102a
Sean Oyler <xrysen@gmail.com>
2020-11-11 02:43:11 +0000

    Added comments to helpers.js

--- helpers.js
+++ helpers.js
@@ -1,3 +1,8 @@
+
+/**
+ * Generates a random string of 6 alphanumeric characters and returns it
+ */
+
 const generateRandomString = () => {
   let output = "";
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -7,15 +12,25 @@ const generateRandomString = () => {
   return output;
 };
 
+ /**
+  * Searches an object for the corresponding email (passed in as a string)
+  * Input:
+  *   - An object
+  *   - A string 
+  * Output:
+  *   - Returns the key of the object whose email property matches the string if found
+  *   - Return false if no match is found 
+  */
+
 const findEmail = (obj, email) => {
   const keys = Object.keys(obj);
-  for (items of keys) {
+  for (let items of keys) {
     if (obj[items].email === email) {
       return items;
     }
   }
   return false;
-}
+};
 
 module.exports = {
   generateRandomString,
